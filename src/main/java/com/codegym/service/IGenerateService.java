45de6b29880678766eb4ae177ea79fd99b7e21eb
LocTM <tranminhloc.anhk@gmail.com>
2024-03-04 08:55:02 +0700

    add CustomerService to service

---
+++ src/main/java/com/codegym/service/IGenerateService.java
@@ -0,0 +1,15 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+
+import java.util.List;
+
+public interface IGenerateService<T> {
+    List<T> findAll();
+
+    void save(T t);
+
+    T findById(Long id);
+
+    void remove(Long id);
+}
